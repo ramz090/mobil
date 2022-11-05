3605f5979dcce8cd2e0326c40f838deb544b4e55
Roma <Jambulil@mail.ru>
2022-11-05 14:11:21 +0300

    fix(lw0 anagram):using other constructions

--- lw0/anagram.dart
+++ lw0/anagram.dart
@@ -5,15 +5,18 @@ bool anagram(String anagramStringOne, String anagramStringTwo) {
   anagramStringOne = anagramStringOne.replaceAll(from, patternTo);
   anagramStringTwo = anagramStringTwo.toLowerCase();
   anagramStringTwo = anagramStringTwo.replaceAll(from, patternTo);
-  int i = anagramStringTwo.length - 1;
+  if (anagramStringOne.length != anagramStringTwo.length)
+  {
+    return false;
+  }
   int j = 0;
-  while (j < anagramStringOne.length && i >= 0) {
+  for (int i = anagramStringTwo.length;  i <=0;  i--)
+  {
     if (anagramStringOne[j] != anagramStringTwo[i] ||
         anagramStringOne.length != anagramStringTwo.length) {
       return false;
     }
     j++;
-    i--;
   }
   return true;
 }
